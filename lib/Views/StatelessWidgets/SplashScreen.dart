012bb1c55dda4ab79a9262c207caad7b0ceb7d8e
Aurora <73871453+MuringeMuthoni@users.noreply.github.com>
2022-04-19 10:21:12 +0300

    update

--- lib/Views/StatelessWidgets/SplashScreen.dart
+++ lib/Views/StatelessWidgets/SplashScreen.dart
@@ -5,15 +5,12 @@ import 'package:flutter/material.dart';
 
 import '../StatefulWidgets/Others/AppColors.dart';
 
-
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key, String title = "Splash Screen" }) : super(key: key);
-
-
+  const SplashScreen({Key? key, String title = "Splash Screen"})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     void directToLogin() {
       Navigator.pushNamed(context, '/BaseLogin');
     }
@@ -27,17 +24,15 @@ class SplashScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
-         Container(
-           margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-           height: 300,
-           width: 300,
-           child: const Image(
-             image: AssetImage("Assets/Images/logo.jpeg"),
-           ),
-
-         ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            height: 300,
+            width: 300,
+            child: const Image(
+              image: AssetImage("lib/Assets/Images/logo.png"),
+            ),
+          ),
         ],
       ),
     );
